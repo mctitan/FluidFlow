9584b48f6472dc6799f28153124a7af77a5e1e38
Colin Alexander Barr <colin.a.barr@gmail.com>
2012-07-18 18:24:04 +0100

    Merge with the code base that I wrote on the laptop

---
+++ FluidFlow/src/net/mctitan/FluidFlow/BlockData.java
@@ -0,0 +1,12 @@
+package net.mctitan.FluidFlow;
+
+import java.io.Serializable;
+
+/**
+ * A place holder for the block data that each fluid may need
+ *
+ * @author mindless728
+ */
+public class BlockData implements Serializable {
+	private static final long serialVersionUID = 1;
+}
